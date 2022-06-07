9c808d1cf14a146784a05bea8ced5957f18f5e34
orlando <orlando.neto@maximatech.com.br>
2022-06-06 22:42:11 -0300

    Inicio
    Criação do projeto

---
+++ src/main/java/com/neto/orlando/CrudSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.neto.orlando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neto/orlando/CrudSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.orlando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
